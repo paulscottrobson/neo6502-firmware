6bf47a1129fd294cdcc8baa7ab3e41433d7b9a54
Paul Robson <credentials.helper>
2023-11-17 16:26:57 +0000

    Update

---
+++ testing/blonk2/build/src/CMakeFiles/blonk.dir/aux/pico/pico-sdk/src/rp2_common/pico_double/double_init_rom.c.obj.d
@@ -0,0 +1,41 @@
+src/CMakeFiles/blonk.dir/aux/pico/pico-sdk/src/rp2_common/pico_double/double_init_rom.c.obj: \
+ /aux/pico/pico-sdk/src/rp2_common/pico_double/double_init_rom.c \
+ /usr/arm-none-eabi/include/string.h /usr/arm-none-eabi/include/_ansi.h \
+ /usr/arm-none-eabi/include/newlib.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/config.h \
+ /usr/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/sys/reent.h \
+ /usr/arm-none-eabi/include/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.0/include/stddef.h \
+ /usr/arm-none-eabi/include/sys/_types.h \
+ /usr/arm-none-eabi/include/machine/_types.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/lock.h \
+ /usr/arm-none-eabi/include/sys/cdefs.h \
+ /usr/arm-none-eabi/include/sys/_locale.h \
+ /usr/arm-none-eabi/include/strings.h \
+ /usr/arm-none-eabi/include/sys/string.h \
+ /aux/pico/pico-sdk/src/rp2_common/pico_bootrom/include/pico/bootrom.h \
+ /aux/pico/pico-sdk/src/common/pico_base/include/pico.h \
+ /aux/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /aux/pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.0/include/stdbool.h \
+ /usr/arm-none-eabi/include/assert.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.0/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /home/paulr/Projects/neo6502-firmware/testing/blonk2/build/generated/pico_base/pico/version.h \
+ /aux/pico/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /home/paulr/Projects/neo6502-firmware/testing/blonk2/build/generated/pico_base/pico/config_autogen.h \
+ /aux/pico/pico-sdk/src/boards/include/boards/pico.h \
+ /aux/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /aux/pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /aux/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /aux/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /aux/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /aux/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /aux/pico/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /aux/pico/pico-sdk/src/rp2_common/pico_bootrom/include/pico/bootrom/sf_table.h
---
+++ testing/blonk2/src/blonk.c
@@ -0,0 +1,43 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
+*/
+
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+
+const uint LED_PIN = 20;
+
+int main() {
+
+    bi_decl(bi_program_description("First Blink"));
+    bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (1) {
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 1);
+        sleep_ms(100);
+    }
+}
