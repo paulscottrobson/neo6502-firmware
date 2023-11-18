1e4ee76791af9d7f607c83c4359ba00181942cf7
Paul Robson <credentials.helper>
2023-11-18 11:09:02 +0000

    Update

--- testing/pico_v4_ide/src/main.cpp
+++ testing/pico_v4_ide/src/main.cpp
@@ -11,6 +11,19 @@
 #include <PicoDVI.h>
 #include "common.h"
 
+#include "Adafruit_TinyUSB.h"
+
+// HID report descriptor using TinyUSB's template
+// Generic In Out with 64 bytes report (max)
+uint8_t const desc_hid_report[] =
+{
+  TUD_HID_REPORT_DESC_GENERIC_INOUT(64)
+};
+
+// USB HID object. For ESP32 these values cannot be changed after this declaration
+// desc report, desc len, protocol, interval, use out endpoint
+Adafruit_USBD_HID usb_hid(desc_hid_report, sizeof(desc_hid_report), HID_ITF_PROTOCOL_NONE, 2, true);
+
 #define FRAMERATE       10 // frames per sec
 #define FRAMETIME     1000 / FRAMERATE  // msec
 #define WIDTH          320
@@ -129,6 +142,8 @@ void setup() {
 
   initDisplay();
   display.print("NEO6502");
+  usb_hid.setReportCallback(get_report_callback, set_report_callback);
+  usb_hid.begin();  
   display.println(" RetroComputer v0.002a");
   initmemory();
   init6502();
@@ -158,4 +173,30 @@ void loop() {
     }
     f = 7500;
   }
+}
+
+
+// Invoked when received GET_REPORT control request
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request
+uint16_t get_report_callback (uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen)
+{
+  // not used in this example
+  (void) report_id;
+  (void) report_type;
+  (void) buffer;
+  (void) reqlen;
+  return 0;
+}
+
+// Invoked when received SET_REPORT control request or
+// received data on OUT endpoint ( Report ID = 0, Type = 0 )
+void set_report_callback(uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize)
+{
+  // This example doesn't use multiple report and report ID
+  (void) report_id;
+  (void) report_type;
+
+  // echo back anything we received from host
+  usb_hid.sendReport(0, buffer, bufsize);
 }
