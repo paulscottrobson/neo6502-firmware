655cbec0ed6d4aa8fdf02c727abe3ad86bbf00e2
Paul Robson <credentials.helper>
2023-11-19 09:53:24 +0000

    Skeleton firmware

---
+++ firmware/build/libraries/PicoDVI/software/libdvi/CMakeFiles/libdvi_dvi_serialiser_pio_h.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for libdvi_dvi_serialiser_pio_h.
+# This may be replaced when dependencies are built.
---
+++ firmware/sources/main.c
@@ -0,0 +1,23 @@
+#include "hardware/gpio.h"
+#include "bsp/board.h"
+#include "tusb.h"
+
+int startVideo();
+void writeCharacter(int n);
+
+int main() {
+	startVideo();
+	board_init();
+	tusb_init();
+    gpio_init(20);
+    gpio_set_dir(20, GPIO_OUT);
+    while (1) {
+        // gpio_put(20, 0);
+        sleep_ms(50);
+        tuh_task();
+        gpio_put(20, 1);
+        // sleep_ms(50);
+  		writeCharacter('.');
+  		tuh_task();
+    }	
+}
