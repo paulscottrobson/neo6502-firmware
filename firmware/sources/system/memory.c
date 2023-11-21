1fb5757a7f6b3b960b0d76e66167ad9627d02495
Paul Robson <credentials.helper>
2023-11-21 06:50:06 +0000

    Commented dvi code

--- firmware/sources/system/memory.c
+++ firmware/sources/system/memory.c
@@ -1,11 +1,28 @@
-// 
-// Author: Rien Matthijsse
-// 
+// ***************************************************************************************
+// ***************************************************************************************
+//
+//      Name :      memory.c
+//      Authors :   Paul Robson (paul@robsons.org.uk)
+//                  Rien Matthijsse
+//      Date :      20th November 2023
+//      Reviewed :  No
+//      Purpose :   Memory initialisation code.
+//
+// ***************************************************************************************
+// ***************************************************************************************
+
 #include "common.h"
-#include "data/binary.h"
 
-uint8_t cpuMemory[MEMORY_SIZE];
+#include "data/binary.h"                                                  // Contains kernel image.
 
+uint8_t cpuMemory[MEMORY_SIZE];                                           // Processor memory
+
+
+// ***************************************************************************************
+//
+//                          Load a binary image into ROM space
+//
+// ***************************************************************************************
 
 static void loadROM(const uint8_t *vROM, uint16_t startAddress, uint16_t romSize) {
   for (uint16_t i = 0; i < romSize; i++) {
@@ -13,7 +30,13 @@ static void loadROM(const uint8_t *vROM, uint16_t startAddress, uint16_t romSize
   }
 }
 
+// ***************************************************************************************
+//
+//                     Initialise CPU memory (call once on boot up)
+//
+// ***************************************************************************************
+
 void MEMInitialiseMemory(void) {
-  loadROM(kernel_bin,KERNEL_LOAD,KERNEL_SIZE);
-  cpuMemory[DEFAULT_PORT] = 0x00;
+  loadROM(kernel_bin,KERNEL_LOAD,KERNEL_SIZE);                            // Load in the kernel
+  cpuMemory[DEFAULT_PORT] = 0x00;                                         // Clear the default command port/
 }
