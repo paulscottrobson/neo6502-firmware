a0db5c814c67ac9e8fcfc42bfc66d4040aee7dcb
Paul Robson <credentials.helper>
2023-12-05 17:36:02 +0000

    Unary/Strings

--- basic/sources/arithmetic/unary/substring.asm
+++ basic/sources/arithmetic/unary/substring.asm
@@ -1,66 +1,43 @@
 ; ************************************************************************************************
 ; ************************************************************************************************
 ;
-;		Name:		dereference.asm
-;		Purpose:	Dereference ... references
-;		Created:	29th November 2023
-;		Reviewed:	No
+;		Name:		substring.asm
+;		Purpose:	LEFT$ RIGHT$ and MID$ 
+;		Created:	5th December 2023
+;		Reviewed:   No
 ;		Author:		Paul Robson (paul@robsons.org.uk)
 ;
 ; ************************************************************************************************
 ; ************************************************************************************************
 
-		.section 	code
+		.section code	
 
 ; ************************************************************************************************
 ;
-;								Dereference a term
+;							LEFT$(a$,n) - left most a$ characters
 ;
 ; ************************************************************************************************
 
-DereferenceTOS:	
-		lda 	XSControl,x 				; check if reference ?
-		and 	#XS_ISREFERENCE
-		beq 	_DRTExit 					; no, exit
-		;
-		lda 	XSNumber0,x 				; copy address to zTemp0
-		sta 	zTemp0
-		lda 	XSNumber1,x
-		sta 	zTemp0+1
-		;
-		lda 	XSControl,x 				; clear reference bits.
-		pha 								; preserve ref bits so we can check byte.
-		and 	#$FF-XS_ISREFERENCE-XS_ISBYTEREFERENCE
-		sta 	XSControl,x
-		pla
-		and 	#XS_ISBYTEREFERENCE
-		bne 	_DRTByteRef
+EXPUnaryLeft: ;; [left$(]
 
-		phy 								; word reference.
-		lda 	(zTemp0)
-		sta 	XSNumber0,x
-		ldy 	#1
-		lda 	(zTemp0),y
-		sta 	XSNumber1,x
-		iny
-		lda 	(zTemp0),y
-		sta 	XSNumber2,x
-		iny
-		lda 	(zTemp0),y
-		sta 	XSNumber3,x
-		ply
-		bra 	_DRTExit
+; ************************************************************************************************
+;
+;							RIGHT$(a$,n) - right most a$ characters
+;
+; ************************************************************************************************
 
-_DRTByteRef:		 						; byte reference
-		lda 	(zTemp0)
-		sta 	XSNumber0,x
-		stz 	XSNumber1,x
-		stz 	XSNumber2,x
-		stz 	XSNumber3,x
-_DRTExit:		
-		rts
+EXPUnaryRight: ;; [right$(]
 
-		.send 		code
+; ************************************************************************************************
+;
+;						MID$(a$,n[,m]) - m characters from position n (or all)
+;
+; ************************************************************************************************
+
+EXPUnaryMid: ;; [mid$(]
+
+
+		.send code
 
 ; ************************************************************************************************
 ;
