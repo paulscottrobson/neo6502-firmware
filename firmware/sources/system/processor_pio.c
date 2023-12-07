30426273444a97ca066db34394e386a646274454
Sascha Schneider <schneider.sascha@mein.gmx>
2023-12-08 00:40:26 +0100

    Optimize and fix pio code.

--- firmware/sources/system/processor_pio.c
+++ firmware/sources/system/processor_pio.c
@@ -4,7 +4,7 @@
 //      Name :      processor_pio.c
 //      Authors :   Sascha Schneider
 //                  Rien Matthijsse
-//      Date :      26th November 2023
+//      Date :      8th December 2023
 //      Reviewed :  No
 //      Purpose :   Drive the 65C02 processor via PIO
 //
@@ -17,11 +17,12 @@
 #define CHIPS_IMPL
 
 #include "system/wdc65C02cpu.h"
-
-uint16_t iCount;
-
-// ***************************************************************************************
 #include "sm0_memory_emulation_with_clock.pio.h"
+
+PIO pio = pio1;
+uint pio_irq = PIO1_IRQ_1;
+uint pio_sm = 0;
+enum pio_interrupt_source irq_source = pis_sm0_rx_fifo_not_empty;
 
 void irq_handler() {
   union u32
@@ -34,9 +35,10 @@ void irq_handler() {
     } data;
   } value;
 
-  value.value = pio1->rxf[0];
+  value.value = pio->rxf[pio_sm];
 
-  bool write = value.data.flags == 0x3;
+  const bool write = (value.data.flags & 0x8) == 0;
+
   if(write)
   {
     *(cpuMemory + value.data.address) = value.data.data;
@@ -44,43 +46,23 @@ void irq_handler() {
 
   uint8_t* address = cpuMemory + value.data.address;
   uint8_t data = *address;
-  pio1->txf[0] = data;
-  iCount++; //tick main loop
-  if ((uint32_t)address == CONTROLPORT && data != 0) {       // Message passed
-    DSPHandler(cpuMemory+controlPort,cpuMemory);   // Go do it. Synchronous, could be Async
-  }
+
+  pio->txf[pio_sm] = data;
 }
 
-void init6502() {
-
-  // RESET
-  //pinMode(uP_RESET, OUTPUT);
-
-  // RW
-  //pinMode(uP_RW, INPUT_PULLUP);
-
-  // PIO
-  PIO pio = pio1;
+void initPio() {
   uint offset = 0;
 
   offset = pio_add_program(pio, &memory_emulation_with_clock_program);
-  //printf("Loaded program at %d\n", offset);
   
-  pio_set_irq0_source_enabled(pio1,  pis_sm0_rx_fifo_not_empty, true);
-  irq_set_exclusive_handler(PIO1_IRQ_0, irq_handler);
-  irq_set_priority(PIO1_IRQ_0, 0);
-  irq_set_enabled(PIO1_IRQ_0, true);
+  pio_set_irq1_source_enabled(pio, irq_source, true);
+  irq_set_exclusive_handler(pio_irq, irq_handler);
+  irq_set_priority(pio_irq, 0);
+  irq_set_enabled(pio_irq, true);
 
-  memory_emulation_with_clock_program_init(pio, 0, offset);
-  pio_sm_set_enabled(pio, 0, true);
-
-  //setReset(false);
-  //sleep_ms(2000);
-  //setReset(true);
+  memory_emulation_with_clock_program_init(pio, pio_sm, offset);
+  pio_sm_set_enabled(pio, pio_sm, true);
 }
-
-// ***************************************************************************************
-
 
 // ***************************************************************************************
 //
@@ -89,11 +71,18 @@ void init6502() {
 // ***************************************************************************************
 
 void CPUExecute(void) {
-    wdc65C02cpu_init();                                                         // Set up the 65C02
-    //wdc65C02cpu_reset();
-    init6502();
+    wdc65C02cpu_init();
+    initPio();
+    wdc65C02cpu_reset();
+
+    uint16_t count = 0;
     while(1) {
-        if (!iCount) DSPSync();                                               // 1 time in 64k. About 25-30Hz.
+      if(cpuMemory[CONTROLPORT] != 0) {
+        DSPHandler(cpuMemory + controlPort, cpuMemory);
+      }
+      if(count++ == 0) {
+          DSPSync();
+      }
     }
 }
 
