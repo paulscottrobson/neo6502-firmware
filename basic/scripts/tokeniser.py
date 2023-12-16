92cdba55f16906df195fcf35ec89cd8a8eea0592
Paul Robson <credentials.helper>
2023-12-16 08:45:20 +0000

    Outline identifier creation code

--- basic/scripts/tokeniser.py
+++ basic/scripts/tokeniser.py
@@ -36,12 +36,11 @@ class IdentifierStore(object):
 	#
 	def add(self,name):
 		name = name.upper()
+		bAddress = len(self.store)
 		assert name not in self.identifiers
 		self.identifiers[name] = len(self.store)+1 											# Points to data, offset 1 in record.
-		isString = name.endswith("$") or name.endswith("$(") 
+		isString = name.endswith("$") or name.endswith("$(") 								# Work out type byte
 		isArray = name.endswith("(")
-		if (len(self.store) & 0xFF) + len(name) + 6 >= 0xFC:								# Close to overflow. $FC in text
-			self.store[0] += 1 																# Another page. 
 		self.store.append(len(name)+6)														# Offset byte
 		self.store += [0,0,0,0] 															# default value
 		ctrl = 0x80 if isString else 0x00
@@ -49,7 +48,11 @@ class IdentifierStore(object):
 		self.store.append(ctrl) 															# control byte.
 		b = [ord(x) for x in name] 															# work out name
 		b[-1] |= 0x80
-		self.store += b																		#  name
+		self.store += b																		# name
+		aAddress = len(self.store)
+		if (bAddress & 0x80) == 0 and (aAddress & 0x80) != 0:  								# creating crossed the middle of the page
+			self.store[0] += 1 																# Another page. 
+
 		return self
 	#
 	def dump(self):
--- basic/sources/editing/tokenise/helpers/tokidfind.asm
+++ basic/sources/editing/tokenise/helpers/tokidfind.asm
@@ -1,10 +1,10 @@
 ; ************************************************************************************************
 ; ************************************************************************************************
 ;
-;		Name:		testing.asmx
-;		Purpose:	Testing code
-;		Created:	29th May 2023
-;		Reviewed:
+;		Name:		tokidfind.asm
+;		Purpose:	Find existing identifier.
+;		Created:	15th December 2023
+;		Reviewed:   No
 ;		Author:		Paul Robson (paul@robsons.org.uk)
 ;
 ; ************************************************************************************************
@@ -12,60 +12,18 @@
 
 		.section code
 
-		.include "testdat.incx"
-
-TestCode:
-		jsr 	PGMNewProgram
-		stz 	$FFFF
-		.include "testrun.incx"
-		jmp 	$FFFF
-
 
 ; ************************************************************************************************
 ;
-;									Process one input line
+;									See if identifier exists					
 ;
 ; ************************************************************************************************
 
-TOKOneLine:
-		stx 	zTemp2
-		sty 	zTemp2+1
-		ldx 	#TOKGetSource & $FF
-		ldy 	#TOKGetSource >> 8
-		sec
-		jsr 	TOKTokenise
-		jsr 	PGMDeleteLine
-		lda 	TOKLineSize
-		cmp 	#4
-		beq 	_TOKNoInsert
-		jsr 	PGMInsertLine
-_TOKNoInsert:
-		rts		
+TOKFindIdentifier:
+		.byte 	3
 
-; ************************************************************************************************
-;
-;		Get data, test for the library, Get CC, Get advance CS, return zero when out of data
-;
-; ************************************************************************************************
-
-TOKGetSource:
-		lda 	(zTemp2)
-		bcc 	_GSNoIncrement
-		inc 	zTemp2
-		bne 	_GSNoIncrement
-		inc 	zTemp2+1
-_GSNoIncrement:
-		cmp 	#0
-		rts
 		.send code
 
-		.section storage
-SrcPosition:
-		.fill 		1				
-		.send storage
-
-
-		
 ; ************************************************************************************************
 ;
 ;									Changes and Updates
