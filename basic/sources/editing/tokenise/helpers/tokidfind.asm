957ce6ad071fc91fa69a1ccaa04149b8cee54860
Paul Robson <credentials.helper>
2023-12-16 21:06:42 +0000

    Tokenisation testing seems to be working

--- basic/sources/editing/tokenise/helpers/tokidfind.asm
+++ basic/sources/editing/tokenise/helpers/tokidfind.asm
@@ -25,7 +25,45 @@ TOKFindIdentifier:
 		ora 	#$80
 		sta 	tokElement,x
 
+		lda 	#Program >> 8				; point zTemp0 to first record.
+		sta 	zTemp0+1
+		lda 	#1
+		sta 	zTemp0
+_TOKFISearch:
+		lda 	(zTemp0)					; reached the end ?
+		beq 	_TOKFIFail
+		;
+		ldy 	#5 							; start matching, pre-increment
+_TOKFIMatch:
+		iny
+		lda 	(zTemp0),y 					; check match
+		cmp 	tokElement-5,y
+		bne 	_TOKFINext 					; no, do next.
+		asl 	a 							; bit 7 set
+		bcc 	_TOKFIMatch 				; no, keep going till a match.
+		;
+		lda 	zTemp0+1 					; XA = record offset
+		sec
+		sbc 	#Program >> 8
+		tax
+		lda 	zTemp0 						
+		inc 	a 							; add 1 to point to data
+		bne 	_TOKFINoCarry
+		inx
+_TOKFINoCarry:
+		sec 								; return success
+		rts		
 
+_TOKFINext:
+		clc 								; go to next record.
+		lda 	(zTemp0)
+		adc 	zTemp0
+		sta 	zTemp0
+		bcc 	_TOKFISearch
+		inc 	zTemp0+1
+		bra 	_TOKFISearch
+
+_TOKFIFail:
 		clc
 		rts
 
