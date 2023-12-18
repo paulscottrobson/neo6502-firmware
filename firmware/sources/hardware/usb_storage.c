3afed9a8643ee3a98d6174d9e2a6c73d0fccf7a6
Paul Robson <credentials.helper>
2023-12-18 16:59:04 +0000

    USB storage nearly there

--- firmware/sources/hardware/usb_storage.c
+++ firmware/sources/hardware/usb_storage.c
@@ -16,80 +16,89 @@
 #include "ff.h"
 #include "diskio.h"
 
-static FATFS msc_fatfs_volumes[CFG_TUH_DEVICE_MAX];
-static volatile bool msc_volume_busy[CFG_TUH_DEVICE_MAX];
+// We are an 8-bit computer, confirm fatfs is too
+static_assert(sizeof(TCHAR) == sizeof(char));
+
+static FATFS msc_fatfs_volumes[FF_VOLUMES];
+static volatile bool msc_volume_busy[FF_VOLUMES];
 static scsi_inquiry_resp_t msc_inquiry_resp;
 
-bool msc_inquiry_complete = false;
-
-bool inquiry_complete_cb(uint8_t dev_addr, tuh_msc_complete_data_t const *cb_data) {
-    if (cb_data->csw->status != 0) {
-        CONWriteString("MSC SCSI inquiry failed\r\n");
+bool inquiry_complete_cb(uint8_t dev_addr, tuh_msc_complete_data_t const *cb_data)
+{
+    if (cb_data->csw->status != 0)
+    {
+        // usb_set_status(dev_addr, "?MSC SCSI inquiry failed");
         return false;
     }
-
-    uint32_t block_count = tuh_msc_get_block_count(dev_addr, cb_data->cbw->lun);
-    uint32_t block_size = tuh_msc_get_block_size(dev_addr, cb_data->cbw->lun);
-    uint32_t size = block_count / ((1024 * 1024) / block_size);
-
-    char szBuffer[64];
-    sprintf(szBuffer,"MSC %luMB %.8s %.16s rev %.4s\r\n", size, msc_inquiry_resp.vendor_id, msc_inquiry_resp.product_id,
-           msc_inquiry_resp.product_rev);
-    CONWriteString(szBuffer);
-
+    const double block_count = tuh_msc_get_block_count(dev_addr, cb_data->cbw->lun);
+    const double block_size = tuh_msc_get_block_size(dev_addr, cb_data->cbw->lun);
+    double size = block_count * block_size / (1024 * 1024);
+    size = ceil(size * 10) / 10;
+    // usb_set_status(dev_addr, "MSC %.1f %s %.8s %.16s rev %.4s",
+    //                size, xb,
+    //                msc_inquiry_resp.vendor_id,
+    //                msc_inquiry_resp.product_id,
+    //                msc_inquiry_resp.product_rev);
     char drive_path[3] = "0:";
     drive_path[0] += dev_addr;
-    FRESULT result = f_mount(&msc_fatfs_volumes[dev_addr], drive_path, 1);
-    if (result != FR_OK) {
-        CONWriteString("MSC filesystem mount failed\r\n");
+    FRESULT mount_result = f_mount(&msc_fatfs_volumes[dev_addr], drive_path, 1);
+    if (mount_result != FR_OK)
+    {
+        // usb_set_status(dev_addr, "?MSC filesystem mount failed (%d)", mount_result);
         return false;
     }
-
+    // If current directory invalid, change to root of this drive
     char s[2];
-    if (FR_OK != f_getcwd(s, 2)) {
+    if (FR_OK != f_getcwd(s, 2))
+    {
         f_chdrive(drive_path);
         f_chdir("/");
     }
-
-    msc_inquiry_complete = true;
     return true;
 }
 
-void tuh_msc_mount_cb(uint8_t dev_addr) {
+void tuh_msc_mount_cb(uint8_t dev_addr)
+{
     uint8_t const lun = 0;
-    CONWriteString("MSC mounted, inquiring\r\n");
+    //usb_set_status(dev_addr, "MSC mounted, inquiring");
     tuh_msc_inquiry(dev_addr, lun, &msc_inquiry_resp, inquiry_complete_cb, 0);
 }
 
-void tuh_msc_umount_cb(uint8_t dev_addr) {
+void tuh_msc_umount_cb(uint8_t dev_addr)
+{
     char drive_path[3] = "0:";
     drive_path[0] += dev_addr;
     f_unmount(drive_path);
 }
 
-static void wait_for_disk_io(BYTE pdrv) {
-    while (msc_volume_busy[pdrv]) {
+static void wait_for_disk_io(BYTE pdrv)
+{
+    while (msc_volume_busy[pdrv])
         tuh_task();
-    };
 }
 
-static bool disk_io_complete(uint8_t dev_addr, tuh_msc_complete_data_t const *cb_data) {
+static bool disk_io_complete(uint8_t dev_addr, tuh_msc_complete_data_t const *cb_data)
+{
     (void)cb_data;
+    CONWriteString("Called IO Complete\r");
     msc_volume_busy[dev_addr] = false;
     return true;
 }
 
-DSTATUS disk_status(BYTE pdrv) {
+DSTATUS disk_status(BYTE pdrv)
+{
     uint8_t dev_addr = pdrv;
     return tuh_msc_mounted(dev_addr) ? 0 : STA_NODISK;
 }
 
-DSTATUS disk_initialize(BYTE pdrv) {
+DSTATUS disk_initialize(BYTE pdrv)
+{
     (void)(pdrv);
     return 0;
 }
 
-DRESULT disk_read(BYTE pdrv, BYTE *buff, LBA_t sector, UINT count) {
+DRESULT disk_read(BYTE pdrv, BYTE *buff, LBA_t sector, UINT count)
+{
     uint8_t const dev_addr = pdrv;
     uint8_t const lun = 0;
     msc_volume_busy[pdrv] = true;
@@ -98,32 +107,40 @@ DRESULT disk_read(BYTE pdrv, BYTE *buff, LBA_t sector, UINT count) {
     return RES_OK;
 }
 
-DRESULT disk_write(BYTE pdrv, const BYTE *buff, LBA_t sector, UINT count) {
+DRESULT disk_write(BYTE pdrv, const BYTE *buff, LBA_t sector, UINT count)
+{
     uint8_t const dev_addr = pdrv;
     uint8_t const lun = 0;
     msc_volume_busy[pdrv] = true;
+    CONWriteString("Writing ");
+    CONWriteHex(count);
+    CONWriteString("\r");
     tuh_msc_write10(dev_addr, lun, buff, sector, (uint16_t)count, disk_io_complete, 0);
+    CONWriteString("Back from write10\r");
     wait_for_disk_io(pdrv);
+    CONWriteString("Back from wait.\r");
     return RES_OK;
 }
 
-DRESULT disk_ioctl(BYTE pdrv, BYTE cmd, void *buff) {
+DRESULT disk_ioctl(BYTE pdrv, BYTE cmd, void *buff)
+{
     uint8_t const dev_addr = pdrv;
     uint8_t const lun = 0;
-    switch (cmd) {
-        case CTRL_SYNC:
-            return RES_OK;
-        case GET_SECTOR_COUNT:
-            *((DWORD *)buff) = (WORD)tuh_msc_get_block_count(dev_addr, lun);
-            return RES_OK;
-        case GET_SECTOR_SIZE:
-            *((WORD *)buff) = (WORD)tuh_msc_get_block_size(dev_addr, lun);
-            return RES_OK;
-        case GET_BLOCK_SIZE:
-            *((DWORD *)buff) = 1;  // 1 sector
-            return RES_OK;
-        default:
-            return RES_PARERR;
+    switch (cmd)
+    {
+    case CTRL_SYNC:
+        return RES_OK;
+    case GET_SECTOR_COUNT:
+        *((DWORD *)buff) = (WORD)tuh_msc_get_block_count(dev_addr, lun);
+        return RES_OK;
+    case GET_SECTOR_SIZE:
+        *((WORD *)buff) = (WORD)tuh_msc_get_block_size(dev_addr, lun);
+        return RES_OK;
+    case GET_BLOCK_SIZE:
+        *((DWORD *)buff) = 1; // 1 sector
+        return RES_OK;
+    default:
+        return RES_PARERR;
     }
 }
 
