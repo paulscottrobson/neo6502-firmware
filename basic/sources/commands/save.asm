73964230662893966f423f8b3cc5bee8a8b77670
Paul Robson <credentials.helper>
2023-12-25 20:46:20 +0000

    Added save of arbitrary memory and file test program

--- basic/sources/commands/save.asm
+++ basic/sources/commands/save.asm
@@ -19,19 +19,18 @@
 		.section code
 
 Command_Save:	;; [save]
-		ldx 	#0
+		ldx 	#0  						; file name
 		jsr 	EXPEvalString
-		lda 	XSNumber0
-		sta 	ControlParameters+0
-		lda 	XSNumber1
-		sta 	ControlParameters+1
+		lda 	(codePtr),y 				; , found
+		cmp 	#KWD_COMMA
+		beq 	_CSSaveRange
 		;
-		stz 	ControlParameters+2
+		stz 	ControlParameters+2 		; start of program
 		lda 	#Program >> 8
 		sta 	ControlParameters+3
 		;
-		jsr 	ClearResetFreeMemory
-		sec
+		jsr 	ClearResetFreeMemory 		; get end of program
+		sec  								; work out length as end-start+1
 		lda 	zTemp0
 		sbc 	ControlParameters+2
 		sta 	ControlParameters+4
@@ -42,11 +41,50 @@ Command_Save:	;; [save]
 		bne 	_CSNoCarry
 		inc 	ControlParameters+5
 _CSNoCarry:		
+		jsr 	_CSSave 					; and do the actual write
+		rts
+;
+;		Save "<name>",<start>,<size>
+;
+_CSSaveRange:
+		inx 								; get from
+		jsr 	ERRCheckComma
+		jsr 	EXPEvalInteger16
+		inx 								; get size
+		jsr 	ERRCheckComma
+		jsr 	EXPEvalInteger16
+		;
+		clc 								; add from to size, error on overflow
+		lda 	XSNumber0+1 				; copy from at the same time.
+		sta 	ControlParameters+2
+		adc 	XSNumber0+2
+		lda 	XSNumber1+1
+		sta 	ControlParameters+3
+		adc 	XSNumber1+2
+		bcs 	_CSError
+		;
+		lda 	XSNumber0+2 				; copy size
+		sta 	ControlParameters+4
+		lda 	XSNumber1+2
+		sta 	ControlParameters+5
+		ora 	XSNumber0+2 				; check size = 0
+		beq 	_CSError
+		jsr 	_CSSave
+		rts
+;
+;		Copy name paramters and do save.
+;
+_CSSave:
+		lda 	XSNumber0
+		sta 	ControlParameters+0
+		lda 	XSNumber1
+		sta 	ControlParameters+1
 		DoSendMessage
 		.byte 	3,3
 		DoWaitMessage
 		lda 	ControlError
 		beq 	_CSExit
+_CSError:		
 		.error_file
 _CSExit		
 		rts
