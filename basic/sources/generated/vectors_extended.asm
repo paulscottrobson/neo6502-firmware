2c3ac08fd1ff2c280471d7a79c6adc93a5f6f76f
Paul Robson <credentials.helper>
2024-01-03 18:37:34 +0000

    Notes

--- basic/sources/generated/vectors_extended.asm
+++ basic/sources/generated/vectors_extended.asm
@@ -27,5 +27,5 @@ ExtendedVectorTable:
 	.word	Command_WHO              ; $195 who
 	.word	Command_Palette          ; $196 palette
 	.word	Unimplemented            ; $197 draw
-	.word	Unimplemented            ; $198 !!un2
+	.word	Unimplemented            ; $198 hide
 	.send code
