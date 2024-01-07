d7ada994c7e14d6d6bf101454080f3d419cc4cba
Paul Robson <credentials.helper>
2024-01-07 18:36:57 +0000

    Early sprite stuff

--- firmware/sources/interface/sprites_xor.c
+++ firmware/sources/interface/sprites_xor.c
@@ -12,6 +12,34 @@
 
 #include "common.h"
 
+static void _SPXORDrawForwardLine(uint8_t bytes,uint8_t *display,uint8_t *image) {
+	while (bytes--) {
+		uint8_t b = *image++;
+		if (b & 0xF0) {
+			*display ^= (b & 0xF0);
+		}
+		display++;
+		if (b & 0x0F) {
+			*display ^= b << 4;
+		}		
+		display++;
+	}
+}
+
+static void _SPXORDrawBackwardLine(uint8_t bytes,uint8_t *display,uint8_t *image) {
+	while (bytes--) {
+		uint8_t b = *image++;
+		--display;
+		if (b & 0xF0) {
+			*display ^= (b & 0xF0);
+		}
+		--display;
+		if (b & 0x0F) {
+			*display ^= b << 4;
+		}		
+	}
+}
+
 // ***************************************************************************************
 //
 //					  Erase sprite : Same as draw as we are XORing
@@ -30,7 +58,18 @@ void SPRPHYErase(SPRITE_INTERNAL *s) {
 
 void SPRPHYDraw(SPRITE_INTERNAL *s) {
 	s->drawAddress = s->x + s->y * gMode.xGSize;  	 							// Work out the draw address top left of sprite.
-	printf("%d %d\n",s->isVisible,s->drawAddress);
+	uint8_t *imgData = gfxMemory + s->imageAddress;  							// Where to get it from.
+	uint8_t yXor = (s->flip & 2) ? (s->ySize-1) : 0;  							// Used to y-flip
+	for (int yPos = 0;yPos < s->ySize;yPos++) {   								// Work top to bottom
+		uint8_t *screenLine = gMode.graphicsMemory + s->drawAddress + (yPos ^ yXor) * gMode.xGSize;
+		if (s->flip & 1) {
+			_SPXORDrawBackwardLine(s->xSize/2,screenLine+s->xSize-1,imgData); 	// Draw a backward line.
+		} else {
+			_SPXORDrawForwardLine(s->xSize/2,screenLine,imgData); 				// Draw a forward line.
+		}
+		imgData += (s->xSize / 2);  											// Next row.
+	}
+	//printf("%d %d\n",s->isVisible,s->drawAddress);
 }
 
 // ***************************************************************************************
