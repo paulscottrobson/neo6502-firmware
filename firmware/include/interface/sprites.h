9c0454ae31b207df66c6c22524c77ca9e711227d
Paul Robson <credentials.helper>
2024-01-07 17:26:52 +0000

    Everything except the drawing

--- firmware/include/interface/sprites.h
+++ firmware/include/interface/sprites.h
@@ -13,9 +13,25 @@
 #ifndef _SPRITES_H
 #define _SPRITES_H
 
-void SPRReset(void);
+#define MAX_SPRITES (128) 														// Max supported. Doesn't mean it's a good idea :)
+
+typedef struct _sprite_internal {
+	bool isDrawn;  																// TRUE if currently drawn
+	bool isVisible;  															// TRUE if currently visible
+	uint16_t x,y;  																// Current drawn position (e.g. passed in)
+	uint16_t xSize,ySize;  														// Sprite horizontal/vertical size.
+	int drawAddress;   															// Physical draw address in screen memory.
+	uint8_t imageSize;  														// image (0:5) size (6) value
+	int imageAddress; 	 														// Physical graphic address in gfxMemory
+	uint8_t flip;  																// flip 0:x 1:y
+} SPRITE_INTERNAL;
+
+void SPRReset(void);  															// Sprite reset.
 void SPRHide(uint8_t *paramData);
-void SPRUpdate(uint8_t *paramData);
+int SPRUpdate(uint8_t *paramData);
+
+void SPRPHYErase(SPRITE_INTERNAL *s); 											// Sprite draw/erase routines
+void SPRPHYDraw(SPRITE_INTERNAL *s);
 
 #endif
 
