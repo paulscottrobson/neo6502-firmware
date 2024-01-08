8c7a6b16e20912d5cc550c16a374598b0ccfb13f
Paul Robson <credentials.helper>
2024-01-08 15:23:38 +0000

    Image creator

---
+++ basic/images/makeimg.py
@@ -0,0 +1,57 @@
+# *******************************************************************************************
+# *******************************************************************************************
+#
+#		Name : 		makeimg.py
+#		Purpose :	Convert images to a graphics data file.
+#		Date :		8th January 2024
+#		Author : 	Paul Robson (paul@robsons.org.uk)
+#
+# *******************************************************************************************
+# *******************************************************************************************
+
+import sys,os
+
+from gconvert import *
+from PIL import Image
+
+# *******************************************************************************************
+#
+#							Create sheet fitting specification
+#
+# *******************************************************************************************
+
+def importSheet(coll,isTile,size,rows):
+	yPaletteSpace = 16 															# Top of screen palette size
+	tileCount = 8 if size == 32 else 16  										# tiles per row
+	width = 8 + (size + 8) * tileCount  										# width of image
+	height = yPaletteSpace + 8 + (size + 8) * rows
+
+	fName = "{0}_{1}.png".format("tile" if isTile else "sprite",size)  			# access the file
+	im = Image.open(fName)  	
+	xt = ImageExtractor(im,None if isTile else (255,0,255))   					# create an extractor.
+	col = 0    																	# scan the file
+	row = 0
+	completed = False
+	count = 0
+
+	while row < rows and not completed:
+		obj = xt.extract(col * (size+8) + 8,row * (size+8) + 8 + yPaletteSpace,size)
+		if not obj.isBlank():
+			count += 1
+			coll.add(obj)
+		else:
+			completed = (col+row) != 0 or not isTile
+		col = col + 1
+		if col == tileCount:
+			col = 0
+			row += 1
+
+	print("\t{0:<7} {1}x{1} found {2}".format("Tiles" if isTile else "Sprites",size,count))
+
+print("Image Set Creator")
+collection = ImageCollection()
+importSheet(collection,True,16,8)
+importSheet(collection,False,16,8)
+importSheet(collection,False,32,4)	
+collection.render("graphics.gfx")
+print("Done.")
