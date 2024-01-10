96ffde8c535af3aa5f4763cae2039370b203c064
Joonas Viskari <joonas.viskari@gmail.com>
2024-01-10 23:56:38 +0200

    fixed build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -6,8 +6,17 @@ else
 fi
 
 pushd firmware/libraries
-git submodule update --init -- fatfs PicoDVI tinyusb
+git submodule update --init -- no-OS-FatFS-SD-SPI-RPi-Pico
 popd
+
+pushd emulator
+make clean
+make 
+popd
+
+pushd basic
+make test
+popd basic
 
 make all
 
