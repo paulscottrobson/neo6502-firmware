97e10ab2fc231d8447a62afd2491543b652e11ab
Paul Robson <credentials.helper>
2024-01-14 14:56:03 +0000

    More tidying up

--- emulator/src/core/sys_debugger.cpp
+++ emulator/src/core/sys_debugger.cpp
@@ -59,65 +59,64 @@ void DBGXRender(int *address,int showDisplay) {
 	char buffer[32];
 	CPUSTATUS *s = CPUGetStatus();
 
-	#ifndef EMSCRIPTEN
+	if (showDisplay == 0) {
+		GFXSetCharacterSize(36,24);
+		DBGVerticalLabel(21,0,labels,DBGC_ADDRESS,-1);									// Draw the labels for the register
 
-	GFXSetCharacterSize(36,24);
-	DBGVerticalLabel(21,0,labels,DBGC_ADDRESS,-1);									// Draw the labels for the register
+		#define DN(v,w) GFXNumber(GRID(24,n++),v,16,w,GRIDSIZE,DBGC_DATA,-1)			// Helper macro
 
-	#define DN(v,w) GFXNumber(GRID(24,n++),v,16,w,GRIDSIZE,DBGC_DATA,-1)			// Helper macro
+		DN(s->a,2);DN(s->x,2);DN(s->y,2);DN(s->pc,4);DN(s->sp+0x100,4);DN(s->status,2);DN(s->cycles,4);
+		DN(s->sign,1);DN(s->overflow,1);DN(s->brk,1);DN(s->decimal,1);DN(s->interruptDisable,1);DN(s->zero,1);DN(s->carry,1);
 
-	DN(s->a,2);DN(s->x,2);DN(s->y,2);DN(s->pc,4);DN(s->sp+0x100,4);DN(s->status,2);DN(s->cycles,4);
-	DN(s->sign,1);DN(s->overflow,1);DN(s->brk,1);DN(s->decimal,1);DN(s->interruptDisable,1);DN(s->zero,1);DN(s->carry,1);
-
-	n = 0;
-	int a = address[1];																// Dump Memory.
-	for (int row = 15;row < 23;row++) {
-		GFXNumber(GRID(0,row),a,16,4,GRIDSIZE,DBGC_ADDRESS,-1);
-		for (int col = 0;col < 8;col++) {
-			int c = CPUReadMemory(a);
-			GFXNumber(GRID(5+col*3,row),c,16,2,GRIDSIZE,DBGC_DATA,-1);
-			c = (c & 0x7F);if (c < ' ') c = '.';
-			GFXCharacter(GRID(30+col,row),c,GRIDSIZE,DBGC_DATA,-1);
-			a = (a + 1) & 0xFFFF;
-		}		
-	}
-
-	int p = address[0];																// Dump program code. 
-	int opc;
-
-	for (int row = 0;row < 14;row++) {
-		int isPC = (p == ((s->pc) & 0xFFFF));										// Tests.
-		int isBrk = (p == address[3]);
-		GFXNumber(GRID(0,row),p,16,4,GRIDSIZE,isPC ? DBGC_HIGHLIGHT:DBGC_ADDRESS,	// Display address / highlight / breakpoint
-																	isBrk ? 0xF00 : -1);
-		opc = CPUReadMemory(p);p = (p + 1) & 0xFFFF;								// Read opcode.
-		strcpy(buffer,_mnemonics[opc]);												// Work out the opcode.
-		char *at = strchr(buffer,'@');												// Look for '@'
-		if (at != NULL) {															// Operand ?
-			char hex[6],temp[32];	
-			if (at[1] == '1') {
-				sprintf(hex,"%02x",CPUReadMemory(p));
-				p = (p+1) & 0xFFFF;
-			}
-			if (at[1] == '2') {
-				sprintf(hex,"%02x%02x",CPUReadMemory(p+1),CPUReadMemory(p));
-				p = (p+2) & 0xFFFF;
-			}
-			if (at[1] == 'r') {
-				int addr = CPUReadMemory(p);
-				p = (p+1) & 0xFFFF;
-				if ((addr & 0x80) != 0) addr = addr-256;
-				sprintf(hex,"%04x",addr+p);
-			}
-			strcpy(temp,buffer);
-			strcpy(temp+(at-buffer),hex);
-			strcat(temp,at+2);
-			strcpy(buffer,temp);
+		n = 0;
+		int a = address[1];																// Dump Memory.
+		for (int row = 15;row < 24;row++) {
+			GFXNumber(GRID(0,row),a,16,4,GRIDSIZE,DBGC_ADDRESS,-1);
+			for (int col = 0;col < 8;col++) {
+				int c = CPUReadMemory(a);
+				GFXNumber(GRID(5+col*3,row),c,16,2,GRIDSIZE,DBGC_DATA,-1);
+				c = (c & 0x7F);if (c < ' ') c = '.';
+				GFXCharacter(GRID(30+col,row),c,GRIDSIZE,DBGC_DATA,-1);
+				a = (a + 1) & 0xFFFF;
+			}		
 		}
-		GFXString(GRID(5,row),buffer,GRIDSIZE,isPC ? DBGC_HIGHLIGHT:DBGC_DATA,-1);	// Print the mnemonic
+
+		int p = address[0];																// Dump program code. 
+		int opc;
+
+		for (int row = 0;row < 14;row++) {
+			int isPC = (p == ((s->pc) & 0xFFFF));										// Tests.
+			int isBrk = (p == address[3]);
+			GFXNumber(GRID(0,row),p,16,4,GRIDSIZE,isPC ? DBGC_HIGHLIGHT:DBGC_ADDRESS,	// Display address / highlight / breakpoint
+																		isBrk ? 0xF00 : -1);
+			opc = CPUReadMemory(p);p = (p + 1) & 0xFFFF;								// Read opcode.
+			strcpy(buffer,_mnemonics[opc]);												// Work out the opcode.
+			char *at = strchr(buffer,'@');												// Look for '@'
+			if (at != NULL) {															// Operand ?
+				char hex[6],temp[32];	
+				if (at[1] == '1') {
+					sprintf(hex,"%02x",CPUReadMemory(p));
+					p = (p+1) & 0xFFFF;
+				}
+				if (at[1] == '2') {
+					sprintf(hex,"%02x%02x",CPUReadMemory(p+1),CPUReadMemory(p));
+					p = (p+2) & 0xFFFF;
+				}
+				if (at[1] == 'r') {
+					int addr = CPUReadMemory(p);
+					p = (p+1) & 0xFFFF;
+					if ((addr & 0x80) != 0) addr = addr-256;
+					sprintf(hex,"%04x",addr+p);
+				}
+				strcpy(temp,buffer);
+				strcpy(temp+(at-buffer),hex);
+				strcat(temp,at+2);
+				strcpy(buffer,temp);
+			}
+			GFXString(GRID(5,row),buffer,GRIDSIZE,isPC ? DBGC_HIGHLIGHT:DBGC_DATA,-1);	// Print the mnemonic
+		}
+		
 	}
-	
-	#endif 
 	renderCount++;
 	if (showDisplay != 0) {
 		int xc = 320;int yc = 240;
