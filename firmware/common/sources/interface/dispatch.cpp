67b62b554a832704168ff6083add4fb8b19762b8
Paul Robson <credentials.helper>
2024-01-24 13:41:04 +0000

    Serial link working at 461kb/s

--- firmware/common/sources/interface/dispatch.cpp
+++ firmware/common/sources/interface/dispatch.cpp
@@ -82,6 +82,7 @@ void DSPReset(void) {
     CONWrite(0x80+6);
     STOSynchronise();                                                           // Synchronise storage
     CONWrite(0x80+2);
+    SERInitialise();                                                            // Initialise serial port.
     CFGProcess();                                                               // Process configuration file.
 }
 
