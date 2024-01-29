185e1f01e88a80bd8b070e9c36e5c252fb5d6609
Sascha Schneider <schneider.sascha@mein.gmx>
2024-01-30 00:29:47 +0100

    Fetch pico-sdk automatically if PICO_SDK_PATH is not set.

--- build_all.sh
+++ build_all.sh
@@ -1,10 +1,3 @@
-if [[ -z "${PICO_SDK_PATH}" ]]; then
-  export PICO_SDK_PATH=/usr/share/pico_sdk
-  echo "PICO_SDK_PATH not set, trying PICO_SDK_PATH=${PICO_SDK_PATH}"
-else
-  echo "PICO_SDK_PATH=${PICO_SDK_PATH}"
-fi
-
 pushd firmware/libraries
 git submodule update --init -- no-OS-FatFS-SD-SPI-RPi-Pico
 popd
