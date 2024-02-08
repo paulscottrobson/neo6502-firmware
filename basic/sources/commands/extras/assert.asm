5e8d708f5ab243167358c9a0a17c4aec9db4bc66
Paul Robson <credentials.helper>
2024-02-08 19:01:12 +0000

    Extended ASSERT

--- basic/sources/commands/extras/assert.asm
+++ basic/sources/commands/extras/assert.asm
@@ -21,10 +21,29 @@
 Command_ASSERT:	;; [assert]
 		ldx 	#0
 		jsr 	EXPEvalNumber 				; get a number to assert
+		stz 	XSNumber0+1 				; zero the string part in case not used.
+		stz 	XSNumber1+1
+		lda 	(codePtr),y 				; check if , present
+		cmp 	#KWD_COMMA
+		bne 	_CANoComma
+		iny 								; consume comma
+		inx
+		jsr 	EXPEvalString 				; get the message to slot #1
+_CANoComma:
+		ldx 	#0
 		jsr 	CheckIfZero
 		beq 	_CAFail 					; if so, the assert fails.
 		rts
 _CAFail:		
+		lda 	XSNumber0+1 				; check if string present
+		ora 	XSNumber1+1
+		beq 	_CAReport
+		lda		XSNumber0+1
+		ldy 	XSNumber1+1
+		jsr 	CPPrintYA
+		lda 	#':'
+		jsr 	CPPrintA
+_CAReport:
 		.error_assert
 
 ; ************************************************************************************************
@@ -55,6 +74,7 @@ _CAType:
 ;
 ;		Date			Notes
 ;		==== 			=====
+;		08-02-24 		Added optional message.
 ;
 ; ************************************************************************************************
 
