af75b8b6dfc29f7bf88c2e21915bb64d6449c0c1
Paul Robson <credentials.helper>
2024-02-29 15:58:01 +0000

    Doc maths

--- firmware/common/include/interface/dispatch.h
+++ firmware/common/include/interface/dispatch.h
@@ -13,6 +13,12 @@
 #ifndef _DISPATCH_H
 #define _DISPATCH_H
 
+#ifdef PICO
+#define WARN_GROUP(group,func) {}
+#else
+#define WARN_GROUP(group,func) DSPWarnHandler(group,func)
+#endif
+
 void DSPHandler(uint8_t *cBlock,uint8_t *memory);
 void DSPSync(void);
 void DSPReset(void);
@@ -23,6 +29,8 @@ uint16_t DSPGetInt16(uint8_t *command,uint8_t paramOffset);
 uint32_t DSPGetInt32(uint8_t *command,uint8_t paramOffset);
 void DSPSetInt16(uint8_t *command,uint8_t paramOffset,uint16_t value);
 void DSPSetInt32(uint8_t *command,uint8_t paramOffset,uint32_t value);
+void DSPWarnHandler(uint8_t group,uint8_t func);
+
 #endif
 
 // ***************************************************************************************
