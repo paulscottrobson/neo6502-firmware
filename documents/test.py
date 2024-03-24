cc29c78aa26f2c059659e0aa48c6c00a4797a7ec
Paul Robson <credentials.helper>
2024-03-24 09:10:41 +0000

    Instr implemented

--- documents/test.py
+++ documents/test.py
@@ -1,6 +1,6 @@
 import serial
 port = serial.Serial("/dev/ttyUSB0")
-port.baudrate = 115200
+port.baudrate = 9600
 for c in range(0,42):
 	for i in range(0,20):
 		port.write(bytearray([(c % 10)+i+97]))
