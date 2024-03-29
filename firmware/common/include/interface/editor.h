9d42e9ee46cf05365934966879a64ea22576fe1a
Paul Robson <credentials.helper>
2024-03-29 21:47:31 +0000

    Editor initial changes

--- firmware/common/include/interface/editor.h
+++ firmware/common/include/interface/editor.h
@@ -16,21 +16,28 @@
 //
 //		States ; it's a sort of FSM, with dropouts.
 //
-#define ES_INITIALISE 		(0) 					// Initialise state. Triggers initialise call out, switch to repaint
-#define ES_REPAINT 			(1) 					// State repaints lines with call out, can be re-entered for multiple lines or switch to edit
-#define ES_EDIT 			(2) 					// Editing state ; left when needs updating, line change etc.
-#define ES_UPDATE 			(3) 					// Update state, call out updates changed lines, 
-#define ES_DISPATCH			(4) 					// Performs required code needed driven by reason for leaving edit state.
-#define ES_EXIT 			(5) 					// Exit state
+#define ES_INITIALISE 		('I')  					// Called when have initialised state.
+#define ES_REPAINT 			('R') 					// State repaints lines with call out, can be re-entered for multiple lines or switch to edit
+#define ES_EDIT 			('E') 					// Editing state ; left when needs updating, line change etc.
+#define ES_UPDATE 			('U') 					// Update state, call out updates changed lines, 
+#define ES_DISPATCH			('D') 					// Performs required code needed driven by reason for leaving edit state.
+#define ES_EXIT 			('X') 					// Exit state
 //
 //		Call out functions.
 //
-#define EX_INITIALISE 		(0) 	 				// Initialise, count lines.
-#define EX_EXIT 			(1)  					// End edit
+#define EX_EXIT 			(0)  					// End edit
+#define EX_INITIALISE 	 	(1)  					// Set up and count lines in editable object.
 #define EX_GETLINE			(2)  					// Get required line into memory in ASCII format detokenising
 #define EX_PUTLINE 			(3)  					// Write line into memory in tokenised form, provided in ASCII
 
-void EDITInitialise(void);
+extern uint8_t 		edState; 	
+extern uint16_t 	edLineCount;
+extern uint8_t  	edWindowTop,edWindowBottom,edWindowLeft,edWindowRight; 				
+extern uint8_t 		edXPos,edYPos;  				
+extern uint16_t  	edTopLine;  					
+extern uint8_t  	edPendingAction;
+
+uint8_t EDITInitialise(void);
 uint8_t EDITContinue(void);
 
 #endif
