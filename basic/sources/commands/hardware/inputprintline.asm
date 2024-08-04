b3d13c7c1038df8ebd555232c34c607418feb690
Paul Robson <credentials.helper>
2024-08-04 08:15:10 +0100

    input line working

--- basic/sources/commands/hardware/inputprintline.asm
+++ basic/sources/commands/hardware/inputprintline.asm
@@ -20,7 +20,52 @@
 
 InputLineHandler:
 		jsr 	IOLCommonChannelCode
-		.byte 	3
+_ILHLoop:
+		lda 	(codePtr),y 				; check for EOL or colon
+		cmp 	#KWD_COLON
+		beq 	_ILHExit
+		cmp 	#KWD_SYS_END
+		beq 	_ILHExit
+
+		ldx	 	#0
+		jsr 	EXPEvaluateExpressionAtX	; get an expression, should be a string variable
+		lda 	XSControl,x 				; check if reference (e.g. a variable) and type together
+		and 	#XS_TYPEMASK|XS_ISVARIABLE
+		cmp  	#XS_ISVARIABLE+XS_ISSTRING
+		bne 	_ILHType 					; if not, then we have an error.
+
+		ldx 	#1 							; slot to read into.
+		lda 	#252 						; maximum string length
+		jsr 	StringTempAllocate 			; allocate space for new string.
+
+_ILHReadLoop:
+		jsr 	IFHReadByte		
+		cmp 	#10 						; LF found, end of string.
+		beq 	_ILHReadDone
+		cmp 	#32 						; 32+ write character to string
+		bcs 	_ILHWriteChar
+		cmp 	#9 							; TAB is converted to space
+		bne 	_ILHNoWrite 				; other controls are ignored
+		lda 	#32
+_ILHWriteChar:		
+		jsr 	StringTempWrite
+_ILHNoWrite:
+		bra 	_ILHReadLoop
+
+_ILHReadDone:
+		ldx 	#0							; now do assignment code
+		jsr 	AssignValueToReference
+
+		lda 	(codePtr),y 				; consume comma if there is one.
+		cmp 	#KWD_COMMA
+		bne 	_ILHLoop
+		iny
+		bra 	_ILHLoop
+_ILHExit:
+		rts
+
+_ILHType:
+		.error_type
 
 ; ************************************************************************************************
 ;
@@ -52,6 +97,7 @@ _OLHWriteLoop:
 _OLHDone:		
 		lda 	#10 						; We use LF to output here
 		jsr 	OFHWriteByte
+
 		lda 	(codePtr),y 				; consume comma if there is one.
 		cmp 	#KWD_COMMA
 		bne 	_OLHLoop
