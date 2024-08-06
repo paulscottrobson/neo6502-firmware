c70df11789c295a49a10086c2487d11f35df8df8
Paul Robson <credentials.helper>
2024-08-06 19:27:07 +0100

    Added PWM interrupt driven code

--- firmware/sources/hardware/sound.cpp
+++ firmware/sources/hardware/sound.cpp
@@ -17,9 +17,9 @@
 #include "dvi.h"
 #include "system/dvi_video.h"
 
-#define SOUND_PIN 	(20) 					// Beeper pin.
+#define AUDIO_PIN 	(20) 					// Beeper pin.
 
-static uint sliceNumber,channel;
+// static uint sliceNumber,channel;
 
 // ***************************************************************************************
 //
@@ -27,19 +27,25 @@ static uint sliceNumber,channel;
 //
 // ***************************************************************************************
 
-static int32_t SNDSetPWMFrequencyDuty(uint slice_num,uint chan,uint32_t f, int d)
-{
-	uint32_t clock = DVI_TIMING.bit_clk_khz * 1024;
-	uint32_t divider16 = clock / f / 4096 + (clock % (f * 4096) != 0);
-	if (divider16 / 16 == 0) divider16 = 16;
-	uint32_t wrap = clock * 16 / divider16 / f - 1;
-	pwm_set_clkdiv_int_frac(slice_num, divider16/16,divider16 & 0xF);
-	pwm_set_wrap(slice_num, wrap);
-	pwm_set_chan_level(slice_num, chan, wrap * d / 100);
-	return wrap;
+// static int32_t SNDSetPWMFrequencyDuty(uint slice_num,uint chan,uint32_t f, int d)
+// {
+// 	// uint32_t clock = DVI_TIMING.bit_clk_khz * 1024;
+// 	// uint32_t divider16 = clock / f / 4096 + (clock % (f * 4096) != 0);
+// 	// if (divider16 / 16 == 0) divider16 = 16;
+// 	// uint32_t wrap = clock * 16 / divider16 / f - 1;
+// 	// pwm_set_clkdiv_int_frac(slice_num, divider16/16,divider16 & 0xF);
+// 	// pwm_set_wrap(slice_num, wrap);
+// 	// pwm_set_chan_level(slice_num, chan, wrap * d / 100);
+// 	// return wrap;
+// }
+
+static int wav_position;
+
+void pwm_interrupt_handler() {
+    wav_position++;
+    pwm_clear_irq(pwm_gpio_to_slice_num(AUDIO_PIN));    
+    pwm_set_gpio_level(AUDIO_PIN,(wav_position & 0x100) ? 0x00:0xFF);
 }
-
-
 
 // ***************************************************************************************
 //
@@ -48,10 +54,25 @@ static int32_t SNDSetPWMFrequencyDuty(uint slice_num,uint chan,uint32_t f, int d
 // ***************************************************************************************
 
 void SNDInitialise(void) {
-	gpio_set_function(SOUND_PIN, GPIO_FUNC_PWM);
-	sliceNumber = pwm_gpio_to_slice_num(SOUND_PIN);
-	channel = pwm_gpio_to_channel(SOUND_PIN);
-	pwm_set_enabled(sliceNumber,false);
+    gpio_set_function(AUDIO_PIN, GPIO_FUNC_PWM);
+    int audio_pin_slice = pwm_gpio_to_slice_num(AUDIO_PIN);
+    // Setup PWM interrupt to fire when PWM cycle is complete
+    pwm_clear_irq(audio_pin_slice);
+    pwm_set_irq_enabled(audio_pin_slice, true);
+    // set the handle function above
+    irq_set_exclusive_handler(PWM_IRQ_WRAP, pwm_interrupt_handler); 
+    irq_set_enabled(PWM_IRQ_WRAP, true);
+    // Setup PWM for audio output
+    pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, 8.0f); 
+    pwm_config_set_wrap(&config, 250); 
+    pwm_init(audio_pin_slice, &config, true);
+    //
+    pwm_set_gpio_level(AUDIO_PIN, 0);
+	// gpio_set_function(SOUND_PIN, GPIO_FUNC_PWM);
+	// sliceNumber = pwm_gpio_to_slice_num(SOUND_PIN);
+	// channel = pwm_gpio_to_channel(SOUND_PIN);
+	// pwm_set_enabled(sliceNumber,false);
 }
 
 // ***************************************************************************************
@@ -61,10 +82,10 @@ void SNDInitialise(void) {
 // ***************************************************************************************
 
 void SNDUpdateSoundChannel(uint8_t channel,SOUND_CHANNEL *c) {
-	if (channel < SOUND_CHANNELS) {
-		SNDSetPWMFrequencyDuty(sliceNumber,channel, c->currentFrequency, 50);
-		pwm_set_enabled(sliceNumber,c->isPlayingNote);
-	}
+	// if (channel < SOUND_CHANNELS) {
+	// 	SNDSetPWMFrequencyDuty(sliceNumber,channel, c->currentFrequency, 50);
+	// 	pwm_set_enabled(sliceNumber,c->isPlayingNote);
+	// }
 }
 
 // ***************************************************************************************
