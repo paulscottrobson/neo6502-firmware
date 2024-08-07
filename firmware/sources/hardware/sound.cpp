9b7e0cdb1d965f5ea1de1e8962eafbfb451925fc
Paul Robson <credentials.helper>
2024-08-07 07:10:11 +0100

    PWM Normal audio working

--- firmware/sources/hardware/sound.cpp
+++ firmware/sources/hardware/sound.cpp
@@ -3,10 +3,9 @@
 //
 //      Name :      sound.cpp
 //      Authors :   Paul Robson (paul@robsons.org.uk)
-//					Harry Fairhead
-//      Date :      21st November 2023
+//      Date :      7th August 2024
 //      Reviewed :  No
-//      Purpose :   Audio support for Neo6502
+//      Purpose :   PWM Audio support for Neo6502
 //
 // ***************************************************************************************
 // ***************************************************************************************
@@ -18,33 +17,31 @@
 #include "system/dvi_video.h"
 
 #define AUDIO_PIN 	(20) 					// Beeper pin.
+#define SAMPLE_DIVIDER (32)
 
-// static uint sliceNumber,channel;
+static int adder = 0;
+static int wrapper = 0;
+static int state = 0;
 
-// ***************************************************************************************
-//
-//						Set up code, borrowed from Harry Fairhead
-//
-// ***************************************************************************************
+static int sampleFrequency = -1;
 
-// static int32_t SNDSetPWMFrequencyDuty(uint slice_num,uint chan,uint32_t f, int d)
-// {
-// 	// uint32_t clock = DVI_TIMING.bit_clk_khz * 1024;
-// 	// uint32_t divider16 = clock / f / 4096 + (clock % (f * 4096) != 0);
-// 	// if (divider16 / 16 == 0) divider16 = 16;
-// 	// uint32_t wrap = clock * 16 / divider16 / f - 1;
-// 	// pwm_set_clkdiv_int_frac(slice_num, divider16/16,divider16 & 0xF);
-// 	// pwm_set_wrap(slice_num, wrap);
-// 	// pwm_set_chan_level(slice_num, chan, wrap * d / 100);
-// 	// return wrap;
-// }
-
-static int wav_position;
+int SNDGetSampleFrequency(void) {
+    if (sampleFrequency < 0) {
+        sampleFrequency = DVI_TIMING.bit_clk_khz * 1024 / SAMPLE_DIVIDER / 255;
+    }
+    return sampleFrequency;
+}
 
 void pwm_interrupt_handler() {
-    wav_position++;
     pwm_clear_irq(pwm_gpio_to_slice_num(AUDIO_PIN));    
-    pwm_set_gpio_level(AUDIO_PIN,(wav_position & 0x100) ? 0x00:0xFF);
+
+    if (adder == 0) return;
+
+    if (wrapper++ >= adder) {
+        wrapper = 0;
+        state = state ^ 0xFF;
+        pwm_set_gpio_level(AUDIO_PIN,state);
+    }
 }
 
 // ***************************************************************************************
@@ -58,21 +55,17 @@ void SNDInitialise(void) {
     int audio_pin_slice = pwm_gpio_to_slice_num(AUDIO_PIN);
     // Setup PWM interrupt to fire when PWM cycle is complete
     pwm_clear_irq(audio_pin_slice);
-    pwm_set_irq_enabled(audio_pin_slice, true);
     // set the handle function above
     irq_set_exclusive_handler(PWM_IRQ_WRAP, pwm_interrupt_handler); 
     irq_set_enabled(PWM_IRQ_WRAP, true);
     // Setup PWM for audio output
     pwm_config config = pwm_get_default_config();
-    pwm_config_set_clkdiv(&config, 8.0f); 
-    pwm_config_set_wrap(&config, 250); 
+    pwm_config_set_clkdiv(&config, SAMPLE_DIVIDER); 
+    pwm_config_set_wrap(&config, 255); 
     pwm_init(audio_pin_slice, &config, true);
     //
     pwm_set_gpio_level(AUDIO_PIN, 0);
-	// gpio_set_function(SOUND_PIN, GPIO_FUNC_PWM);
-	// sliceNumber = pwm_gpio_to_slice_num(SOUND_PIN);
-	// channel = pwm_gpio_to_channel(SOUND_PIN);
-	// pwm_set_enabled(sliceNumber,false);
+    pwm_set_irq_enabled(audio_pin_slice, true);
 }
 
 // ***************************************************************************************
@@ -82,16 +75,16 @@ void SNDInitialise(void) {
 // ***************************************************************************************
 
 void SNDUpdateSoundChannel(uint8_t channel,SOUND_CHANNEL *c) {
-	// if (channel < SOUND_CHANNELS) {
-	// 	SNDSetPWMFrequencyDuty(sliceNumber,channel, c->currentFrequency, 50);
-	// 	pwm_set_enabled(sliceNumber,c->isPlayingNote);
-	// }
+    if (c->isPlayingNote) {    
+        adder = SNDGetSampleFrequency() / c->currentFrequency / 2;
+    } else {
+        adder = 0;
+    }
 }
 
 // ***************************************************************************************
 //
 //		Date 		Revision
 //		==== 		========
-//		16-01-24	Moved tick callback stuff to tick source for generalised usage
 //
 // ***************************************************************************************
